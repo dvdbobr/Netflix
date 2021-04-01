de98feb091b93889e007d6e3107b5437d0104570
David Bobritsky <dvdbobr@gmail.com>
2021-04-01 14:47:33 +0300

    first commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+export default function Main() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get('https://api.themoviedb.org/3/movie/76341?api_key=a3d71a761a7bb30717e08b95a73a97c4')
+            console.log(response.data);
+           
+        }
+        getData()
+    }, [])
+    return (
+        <div>
+
+        </div>
+    )
+}
