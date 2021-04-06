c3f56740f94c072607d4037914441534a786ee98
David Bobritsky <dvdbobr@gmail.com>
2021-04-07 01:04:36 +0300

    search update movies and series

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -6,22 +6,30 @@ import Row from './Row'
 export default function Search(props) {
     const endpoint = 'https://api.themoviedb.org/3'
     const imgEndpoint = 'http://image.tmdb.org/t/p/original'
-    const [searchList, setSearchList] = useState([])
+    const [searchMovies, setMovies] = useState([])
+    const [searchSeries, setSearchSeries] = useState([])
     useEffect(() => {
         const searchMovie = async () => {
-            const response = await axios.get(`${endpoint}/search/movie?api_key=a3d71a761a7bb30717e08b95a73a97c4&query=${props.match.params.term}`)
-            console.log(response.data.results);
-            setSearchList(response.data.results)
+            const movies = await axios.get(`${endpoint}/search/movie?api_key=a3d71a761a7bb30717e08b95a73a97c4&query=${props.match.params.term}`)
+            const series = await axios.get(`${endpoint}/search/tv?api_key=a3d71a761a7bb30717e08b95a73a97c4&query=${props.match.params.term}`)
+
+            console.log(series.data.results);
+            setSearchSeries(series.data.results)
+            setMovies(movies.data.results)
         }
         searchMovie()
-    }, [])
+    }, [props.match.params.term])
     return (
         <div>
             <Navbar />
             <div className="searchMovies">
                 <Row title={`MOVIES RELATED TO ${(props.match.params.term).toUpperCase()}`}
                     isMovieOrTV={'movie'}
-                    data={searchList}
+                    data={searchMovies}
+                    imgEndpoint={imgEndpoint} />
+                <Row title={`TV SHOWS RELATED TO ${(props.match.params.term).toUpperCase()}`}
+                    isMovieOrTV={'tv'}
+                    data={searchSeries}
                     imgEndpoint={imgEndpoint} />
             </div>
         </div>
